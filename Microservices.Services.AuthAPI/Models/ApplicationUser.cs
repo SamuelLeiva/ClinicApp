0a1f72b2d45a0473345cc80e98ac64933461d552
SamuelLeiva <leiva20152@gmail.com>
2025-08-05 12:15:31 -0500

    chore: program.cs modified and first migration applied.

--- Microservices.Services.AuthAPI/Models/ApplicationUser.cs
+++ Microservices.Services.AuthAPI/Models/ApplicationUser.cs
@@ -4,6 +4,6 @@ namespace Microservices.Services.AuthAPI.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string Name { get; set; }
+        public string Names { get; set; }
     }
 }
