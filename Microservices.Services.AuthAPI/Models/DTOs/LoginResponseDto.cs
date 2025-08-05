cfd7f95f77bd5c9c874e8c7255804e19a89db3f0
SamuelLeiva <leiva20152@gmail.com>
2025-08-05 11:49:17 -0500

    chore: Request's and response's Dtos added.

---
+++ Microservices.Services.AuthAPI/Models/DTOs/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Microservices.Services.AuthAPI.Models.DTOs
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
